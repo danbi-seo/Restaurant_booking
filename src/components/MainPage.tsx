7cba77d1029015242f85e554769df12e79510807
danbi-seo <sdb0504@naver.com>
2025-08-29 00:30:36 +0900

    chore: add mainposter2.jpeg for mainPage

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import mainPoster from "../assets/img/mainposter.jpg";
+import mainPoster from "../assets/img/main_poster.jpg";
+import mainPoster2 from "../assets/img/main_poster2.jpeg";
 
 const MainPage = () => {
   return (
-    <section className="w-full min-h-screen bg-black flex justify-center">
+    <section className="w-full min-h-screen bg-black flex flex-col justify-center ">
       <img
         src={mainPoster}
         alt="메인 포스터"
-        className="w-[70%] h-auto self-start mx-auto"
+        className="w-[60%] h-auto self-start mx-auto"
+      />
+      <img
+        src={mainPoster2}
+        alt="메인 포스터"
+        className="w-[100%] h-auto self-start mx-auto"
       />
     </section>
   );
